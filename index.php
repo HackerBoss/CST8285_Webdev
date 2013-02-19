6e64f7f952ae712901baf3b90f41238e9e10dd8c
HackerBoss <tikkiray@hotmail.co.il>
2013-02-19 12:31:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,10 @@ $no_db = false;
 		<title>Katata Networks - learning jQuery - Kyryl Afanasenko</title>
 		<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon"> 
 		<link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon"> 
-		<link rel="stylesheet" type="text/css" media="screen" href="assets/css/index.css">
-		<link rel="stylesheet" href="includes/jQuery.isc/jQuery.isc.css" type="text/css" media="screen" charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="includes/jFormer/jformer.css" ></link>
+		<style>
+		h1 {color:red;}
+		</style>
+		
 		<?php 
 			if(isset($_GET['op'])){
 				$op = $_GET['op'];
@@ -67,112 +68,6 @@ $no_db = false;
 		</script>
 	</head>
 	<body>
-		<table id="container">
-			<tr>
-				<td class="centered">
-					<table id="header">
-						<tr>
-							<td id="logo_box" rowspan="2"><a href="index.php"><img src="assets/images/logo.png"></a></td>
-							<td id="page_title"><h1>
-								Katata Networks 
-							</h1></td>
-						</tr>
-						<tr>
-							<td class="center">
-								<table class="links">
-									<tr>
-										<td class="links"><a class="menu" href="index.php">Home</a></td><td> | </td>
-										<td class="links"><a class="menu" href="index.php?op=about">About Me</a></td><td> | </td>
-										<td class="links"><a class="menu" href="index.php?op=register">Register</a></td><td> | </td>
-										<td class="links"><a class="menu" href="index.php?op=downloads">Downloads</a></td><td> | </td>
-										<td class="links"><a class="menu" href="index.php?op=pictures">Pictures</a></td><td> | </td>
-										<td class="links">
-											<div id="drop_down">
-												<ul>
-													<li  class="parent">
-														<a href="#">Labs</a>
-														<ul>
-															<li>
-																<a href="index.php?op=lab2">Lab 2</a>
-															</li>
-															<li>
-																<a href="index.php?op=lab3">Lab 3</a>
-															</li>
-															<li>
-																<a href="index.php?op=lab4">Lab 4</a>
-															</li>
-															<li>
-																<a href="index.php?op=lab5">Lab 5</a>
-															</li>
-														</ul>
-													</li>
-												</ul>
-											</div>
-										</td>
-										
-										<td> | </td><td class="links"><a class="menu" href="index.php?op=testing">Test</a></td>
-										<td> | </td><td class="links"><a class="menu" href="index.php">Placeholder</a></td>
-										<td> | </td><td class="links"><a class="menu" href="index.php">Placeholder</a></td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-					</table>
-					<div id="photo_container"></div>
-					<table>
-						<tr>
-							<td id="left_container">
-								<?php
-								include_once("includes/login/include/constants.php");
-								error_reporting(E_ALL);
-								if(mysql_connect(DB_SERVER, DB_USER, DB_PASS)){
-									include("modules/login_form.php"); 
-								} else {
-									echo("no db connection. login disabled. sowwy");
-									$no_db = true;
-								}
-								?>
-							</td>
-							<td id="right_container">
-								<!-- inner container include. -->
-								 <?php
-									if (!isset($_GET['op'])) { 
-										include("modules/index.php"); 
-									} else {
-									  	$op = $_GET['op'];
-								      	if (is_file("modules/".$op.".php")) {
-								      		include("modules/".$op.".php");
-								      	} else {	
-											echo ("<div id='error'>Module could not be found!<br/></div>");
-								      	}
-									} 
-								 ?>
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-		</table>
-		<br />
-		</div>
-		<div id="footer">
-    		Katata Networks © 2013 All rights reserved.  <!--<img src="assets/images/valid-html40.gif"> -->
-    		<?php
-	    		if($no_db == false){
-		    		$a_u = $database->num_active_users;
-		    		$a_g = $database->num_active_guests;
-					echo " | Members Total: ".$database->getNumMembers();
-					echo " | There ";
-						if($a_u == 1){echo"is";}else{echo"are";}
-					echo " $a_u ";
-						if($a_u == 1){echo"member";}else{echo"members";}
-					echo " and ";
-					echo "$a_g ";
-						if($a_g == 1){echo"guest";}else{echo"guests";}
-					echo " viewing the site.";
-					//include("includes/login/include/view_active.php");  <-- lists all logged in active users, by username
-				}
-			?>
-		</div>
+		<h1>SUCK IT</h1>
 	</body>
 </html>
